114dd2784fb91f1a579d6e8d3e7d982714751000
Paula Jach <paulajach92@gmail.com>
2020-03-18 15:00:28 +0100

    created navbar for mobile devices

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import '../src/scss/styles.scss';
 
 
+import "particles.js";
+
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
+
+const menuIcon = document.querySelector('.hamburger');
+const navbar = document.querySelector('.header__nav--mobile');
+  
+menuIcon.addEventListener('click', () => {
+  console.log('click')
+    navbar.classList.toggle('change');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,54 @@
-const path = require('path');
-const webpack = require('webpack');
-const CopyPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const autoprefixer = require('autoprefixer');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ManifestPlugin = require('webpack-manifest-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const autoprefixer = require("autoprefixer");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ManifestPlugin = require("webpack-manifest-plugin");
 
 module.exports = {
-    module: {
-        rules: [{
-            test: /\.s[ac]ss$/i,
-            use: [
-                // Creates `style` nodes from JS strings
-                MiniCssExtractPlugin.loader,
-                // Translates CSS into CommonJS
-                'css-loader?url=false',
-                // Compiles Sass to CSS
-                'sass-loader',
-            ],
-        }, ],
-    },
-    mode: "production",
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            hash: true,
-            filename: 'index.html',
-            template: 'index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-        }),
-        new CopyPlugin([{
-            from: 'img',
-            to: 'img'
-        },
-     ]),
-	new ManifestPlugin(),
-    ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader?url=false",
+          // Compiles Sass to CSS
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      filename: "index.html",
+      template: "index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    }),
+    new CopyPlugin([
+      {
+        from: "img",
+        to: "img"
+      }
+    ]),
+    new CopyPlugin([
+        {
+          from: "assets",
+          to: "assets"
+        }
+      ]),
+    new ManifestPlugin()
+  ]
 };
