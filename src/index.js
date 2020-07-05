1dae348ff855d509db3a746005045f3a5034b0dc
Paula Jach <paulajach92@gmail.com>
2020-07-05 20:37:11 +0200

    used js-beautify to improve code structure

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import 'jquery';
 // particlesJS.load(@dom-id, @path-json, @callback (optional))
 particlesJS.load('particles-js', 'assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
-  });
+});
 
 
 // open mobile menu
 const menuIcon = document.querySelector('.hamburger');
 const navbar = document.querySelector('.header__nav--mobile');
-  
+
 menuIcon.addEventListener('click', () => {
     navbar.classList.toggle('change');
     navbar.classList.add('open');
@@ -21,43 +21,43 @@ menuIcon.addEventListener('click', () => {
 
 //close nav after click
 $(document).ready(function() {
-  $(document).click(function(event) {
-      const clickover = $(event.target);
-      const opened = $(".header__nav--mobile").hasClass("open");
-      if (opened === true && !clickover.hasClass("hamburger")) {
-          $(".hamburger").click();
-      }
-  });
+    $(document).click(function(event) {
+        const clickover = $(event.target);
+        const opened = $(".header__nav--mobile").hasClass("open");
+        if (opened === true && !clickover.hasClass("hamburger")) {
+            $(".hamburger").click();
+        }
+    });
 
 });
 
 //Smooth scrolling
 $(document).ready(function() {
-  $("a.scrollable").on('click', function(event) {
-      if (this.hash !== "") {
-          event.preventDefault();
-          const hash = this.hash;
-          $('html, body').animate({
-              scrollTop: $(hash).offset().top
-          }, 1000, function() {
-              window.location.hash = hash;
-          });
-      }
-  });
+    $("a.scrollable").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            const hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
 });
 
 
 // change header bg-color after scrolling 
 $(document).ready(function() {
-  $(window).scroll(function() {
-      if ($(this).scrollTop() > 500) {
-        $('.header__nav').addClass('solid');
-        $('.header__nav--mobile').addClass('solid');
-        $('.header__nav--mobile .hamburger .line').css('background-color', '#1d2935');
-      } else {
-        $('.header__nav').removeClass('solid');
-        $('.header__nav--mobile').removeClass('solid');
-        $('.header__nav--mobile .hamburger .line').css('background-color', 'white');
-      }
-  });
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 500) {
+            $('.header__nav').addClass('solid');
+            $('.header__nav--mobile').addClass('solid');
+            $('.header__nav--mobile .hamburger .line').css('background-color', '#1d2935');
+        } else {
+            $('.header__nav').removeClass('solid');
+            $('.header__nav--mobile').removeClass('solid');
+            $('.header__nav--mobile .hamburger .line').css('background-color', 'white');
+        }
+    });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -9,64 +9,59 @@ const $ = require('jquery');
 
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          MiniCssExtractPlugin.loader,
-          // Translates CSS into CommonJS
-          "css-loader?url=false",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ]
-      },
-      {
-      test: require.resolve('jquery'),
-      use: [{
-        loader: 'expose-loader',
-        options: 'jQuery'
-      },{
-        loader: 'expose-loader',
-        options: '$'
-      }]
-      }
-      
-    ]
-  },
+    module: {
+        rules: [{
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    MiniCssExtractPlugin.loader,
+                    // Translates CSS into CommonJS
+                    "css-loader?url=false",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ]
+            },
+            {
+                test: require.resolve('jquery'),
+                use: [{
+                    loader: 'expose-loader',
+                    options: 'jQuery'
+                }, {
+                    loader: 'expose-loader',
+                    options: '$'
+                }]
+            }
 
-  mode: "production",
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.js"
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      hash: true,
-      filename: "index.html",
-      template: "index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css"
-    }),
-    new CopyPlugin([
-      {
-        from: "img",
-        to: "img"
-      }
-    ]),
-    new CopyPlugin([
-        {
-          from: "assets",
-          to: "assets"
-        }
-      ]),
-      new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery'
-      }),
-    new ManifestPlugin()
-  ]
+        ]
+    },
+
+    mode: "production",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            filename: "index.html",
+            template: "index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        }),
+        new CopyPlugin([{
+            from: "img",
+            to: "img"
+        }]),
+        new CopyPlugin([{
+            from: "assets",
+            to: "assets"
+        }]),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+        new ManifestPlugin()
+    ]
 };
